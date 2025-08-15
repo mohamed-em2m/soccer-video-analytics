447ab4629fff981e4c9343ab9874485f5fa4b7ac
Mohamed Emam <126331291+mohamed-em2m@users.noreply.github.com>
2025-08-15 20:03:07 +0300

    Update colors.py

--- inference/colors.py
+++ inference/colors.py
@@ -1,60 +1,3 @@
-white = {
-    "name": "white",
-    "lower_hsv": (0, 0, 184),
-    "upper_hsv": (179, 39, 255),
-}
-
-red = {
-    "name": "red",
-    "lower_hsv": (0, 100, 0),
-    "upper_hsv": (8, 255, 255),
-}
-
-blueish_red = {
-    "name": "blueish_red",
-    "lower_hsv": (170, 0, 0),
-    "upper_hsv": (178, 255, 255),
-}
-
-orange = {
-    "name": "orange",
-    "lower_hsv": (7, 178, 0),
-    "upper_hsv": (15, 255, 255),
-}
-
-yellow = {
-    "name": "yellow",
-    "lower_hsv": (23, 0, 0),
-    "upper_hsv": (29, 255, 255),
-}
-
-green = {
-    "name": "green",
-    "lower_hsv": (48, 50, 0),
-    "upper_hsv": (55, 255, 255),
-}
-
-sky_blue = {
-    "name": "sky_blue",
-    "lower_hsv": (95, 38, 0),
-    "upper_hsv": (111, 190, 255),
-}
-
-blue = {
-    "name": "blue",
-    "lower_hsv": (112, 80, 0),
-    "upper_hsv": (126, 255, 255),
-}
-
-black = {
-    "name": "black",
-    "lower_hsv": (0, 0, 0),
-    "upper_hsv": (179, 255, 49),
-}
-
-all = [white, red, orange, yellow, green, sky_blue, blue, blueish_red, black]
-# HSV color ranges for OpenCV (H: 0–179, S: 0–255, V: 0–255)
-
 color_ranges = [
     {
         "name": "white",
@@ -136,3 +79,4 @@ color_ranges = [
 
 # If you want them all in one dictionary by name:
 color_dict = {c["name"]: c for c in color_ranges}
+
